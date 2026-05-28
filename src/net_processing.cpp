nmfretz/libre-relay-bitcoind

Peer with 4 other Libre Relay nodes
Also, set FULL_RBF nService bit if full-rbf enabled.

src/net_processing.cpp
@@ -3758,6 +3758,13 @@ void PeerManagerImpl::ProcessMessage(CNode& pfrom, const std::string& msg_type, 
             return;
         }
 
+        if (pfrom.IsLibreRelay() && !HasLibreRelayServiceFlag(nServices))
+        {
+            LogPrint(BCLog::NET, "peer=%d does not offer libre relay as expected; disconnecting\n", pfrom.GetId());
+            pfrom.fDisconnect = true;
+            return;
+        }
+
         if (nVersion < MIN_PEER_PROTO_VERSION) {
             // disconnect from peers older than this proto version
             LogPrint(BCLog::NET, "peer=%d using obsolete version %i; disconnecting\n", pfrom.GetId(), nVersion);

test/functional/p2p_node_network_limited.py
@@ -14,6 +14,8 @@ from test_framework.messages import (
     NODE_NETWORK_LIMITED,
     NODE_P2P_V2,
     NODE_WITNESS,
+    NODE_FULL_RBF,
+    NODE_LIBRE,
     msg_getdata,
 )
 from test_framework.p2p import P2PInterface
@@ -118,7 +120,7 @@ class NodeNetworkLimitedTest(BitcoinTestFramework):
     def run_test(self):
         node = self.nodes[0].add_p2p_connection(P2PIgnoreInv())
 
-        expected_services = NODE_WITNESS | NODE_NETWORK_LIMITED
+        expected_services = NODE_WITNESS | NODE_NETWORK_LIMITED | NODE_FULL_RBF | NODE_LIBRE
         if self.options.v2transport:
             expected_services |= NODE_P2P_V2
 

privkeyio/bitcoin

src/net_processing.cpp
@@ -3476,6 +3476,13 @@ void PeerManagerImpl::ProcessMessage(CNode& pfrom, const std::string& msg_type, 
             return;
         }
 
+        if (pfrom.IsLibreRelay() && !HasLibreRelayServiceFlag(nServices))
+        {
+            LogDebug(BCLog::NET, "peer=%d does not offer libre relay as expected; disconnecting\n", pfrom.GetId());
+            pfrom.fDisconnect = true;
+            return;
+        }
+
         if (nVersion < MIN_PEER_PROTO_VERSION) {
             // disconnect from peers older than this proto version
             LogDebug(BCLog::NET, "peer using obsolete version %i, %s\n", nVersion, pfrom.DisconnectMsg(fLogIPs));

test/functional/p2p_node_network_limited.py
@@ -15,6 +15,7 @@ from test_framework.messages import (
     NODE_NETWORK_LIMITED,
     NODE_P2P_V2,
     NODE_WITNESS,
+    NODE_PREFERENTIAL_PEERING,
     msg_getdata,
 )
 from test_framework.p2p import P2PInterface
@@ -123,7 +124,7 @@ class NodeNetworkLimitedTest(BitcoinTestFramework):
     def run_test(self):
         node = self.nodes[0].add_p2p_connection(P2PIgnoreInv())
 
-        expected_services = NODE_WITNESS | NODE_NETWORK_LIMITED | NODE_REDUCED_DATA
+        expected_services = NODE_WITNESS | NODE_NETWORK_LIMITED | NODE_REDUCED_DATA | NODE_PREFERENTIAL_PEERING
         if self.options.v2transport:
             expected_services |= NODE_P2P_V2
 